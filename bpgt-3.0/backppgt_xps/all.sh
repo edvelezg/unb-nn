8248de6a93707f17f7ac0f3bb45d6eeec273f51e
EdVelez.G <EdVelez.G@9a33703e-7ef9-c3aa-938f-e7d449b18700>
2010-11-26 04:31:18 +0000

    Working Copy

---
+++ bpgt-3.0/backppgt_xps/all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+./genInput.rb
+
+arr=(5 10 15 20 100 500 5000 10000 20000 40000)
+INPUTS=""
+for (( i = 0; i < ${#arr[*]}; i++ )); do
+	./main.rb ${arr[i]}
+	cp output.txt output_${arr[i]}.txt
+	cp training.txt training_${arr[i]}.txt
+	./genPlots.sh
+	mv output.eps ../graphs/output_${arr[i]}.eps
+	mv training.eps ../graphs/training_${arr[i]}.eps
+done
+
+dir=`pwd`
+echo -e "Hi,\n\n Your script in $dir is done ;)" | mail -s "Job's Done" "edvelez.g@gmail.com"
